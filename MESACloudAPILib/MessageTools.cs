5f61de62a827f7896e33a2a9928d01d60343ec7b
ligengjun <hnlylgj@qq.com>
2018-01-25 18:03:11 +0800

    20180125-2

---
+++ MESACloudAPILib/MessageTools.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MESACloudAPILib
+{
+    public class MessageTools
+    {
+        int id;
+
+    }
+}
